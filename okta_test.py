8872b1d1598035cf6c52f099837f7afdc1567bee
Semona Igama <semona.igama@gmail.com>
2020-03-16 13:14:34 -0700

    renamed file, added script to fetch account info

--- okta_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-resp = requests.get('https://semonav2.oktapreview.com')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-# for profile in resp.json():
-#     print('{} {}'.format(profile['firstName'], profile['lastName']))
-    print(resp.json)
